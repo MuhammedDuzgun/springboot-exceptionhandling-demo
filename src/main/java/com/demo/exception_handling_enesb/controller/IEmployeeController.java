241a9ce0df3f459aae83d6d3919188766d91d8ae
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-10-19 00:45:16 +0300

    add Employee Controller Interface

--- src/main/java/com/demo/exception_handling_enesb/controller/IEmployeeController.java
+++ src/main/java/com/demo/exception_handling_enesb/controller/IEmployeeController.java
@@ -1,7 +1,8 @@
 package com.demo.exception_handling_enesb.controller;
 
 import com.demo.exception_handling_enesb.dto.EmployeeDto;
+import com.demo.exception_handling_enesb.entity.RootEntity;
 
 public interface IEmployeeController {
-    public EmployeeDto getEmployeeById(Long id);
+    public RootEntity<EmployeeDto> getEmployeeById(Long id);
 }
