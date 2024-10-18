1cbc1b71a222abc7be0f519edb92239a40d2b5fb
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-10-18 21:21:09 +0300

    Initial commit

---
+++ src/main/java/com/demo/exception_handling_enesb/ExceptionHandlingEnesbApplication.java
@@ -0,0 +1,13 @@
+package com.demo.exception_handling_enesb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionHandlingEnesbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionHandlingEnesbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/exception_handling_enesb/ExceptionHandlingEnesbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.exception_handling_enesb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionHandlingEnesbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
