ba3d8b67f544030f6054073364c54491ab3d50a0
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-10-19 00:37:01 +0300

    add Root entity

---
+++ src/main/java/com/demo/exception_handling_enesb/entity/RootEntity.java
@@ -0,0 +1,34 @@
+package com.demo.exception_handling_enesb.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RootEntity<T> {
+
+    private boolean result;
+    private String errorMessage;
+    private T data;
+
+    public static  <T> RootEntity<T> ok(T data) {
+        RootEntity<T> rootEntity = new RootEntity<T>();
+        rootEntity.setResult(true);
+        rootEntity.setErrorMessage(null);
+        rootEntity.setData(data);
+        return rootEntity;
+    }
+
+    public static  <T> RootEntity<T> error(String errorMessage) {
+        RootEntity<T> rootEntity = new RootEntity<T>();
+        rootEntity.setResult(false);
+        rootEntity.setErrorMessage(errorMessage);
+        rootEntity.setData(null);
+        return rootEntity;
+    }
+
+}
