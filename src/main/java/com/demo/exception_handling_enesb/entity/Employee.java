383746eb178330026463168c129584a13630e9f9
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-10-18 21:40:57 +0300

    add Department and Employee entities

---
+++ src/main/java/com/demo/exception_handling_enesb/entity/Employee.java
@@ -0,0 +1,26 @@
+package com.demo.exception_handling_enesb.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    private Department department;
+
+}
